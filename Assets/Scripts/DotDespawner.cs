2db660bae9e2b37024cdd5b3fda9e14c4bd90c27
Andrej Šebeň <andrej.seben@millennium.sk>
2018-12-13 16:56:00 +0100

    Initial commit

---
+++ Assets/Scripts/DotDespawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DotDespawner : MonoBehaviour
+{
+    public int zostavajuceZivotyPocet;
+    public Text zivotyText;
+    public Text cas;
+    public Text koniecHry;
+    public Button hratZnova;
+    public bool koniec = false;
+
+    void Start()
+    {
+        koniecHry.enabled = false;
+        hratZnova.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Dot")
+        {
+            Destroy(collision.gameObject);
+            zostavajuceZivotyPocet--;
+        }
+        //Debug.Log(pocetNechytenych);
+
+    }
+
+    void Update()
+    {
+        if(!koniec)
+        { 
+            zivotyText.text = "Zostávajúce životy: " + zostavajuceZivotyPocet;
+            cas.text = "Čas hrania: " + Time.time;
+
+            if(zostavajuceZivotyPocet == 0 && koniec == false )
+            {
+                koniecHry.enabled = true;
+                koniecHry.text = "Koniec hry! \n Celková dĺžka hrania:\n" + Time.time;
+                koniec = true;
+                hratZnova.enabled = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DotSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DotSpawner : MonoBehaviour
+{
+    public GameObject dot;
+    float randomX;
+    Vector2 spawnPoint;
+    public float rangeLow, rangeHigh;
+    public float respawnTime;
+    float nextSpawn;
+
+    public DotDespawner despawner;
+    void Update()
+    {
+        if (Time.time > nextSpawn && despawner.koniec == false)
+        {
+            nextSpawn = Time.time + respawnTime;
+            randomX = Random.Range(rangeLow, rangeHigh);
+            spawnPoint = new Vector2(randomX, transform.position.y);
+            Instantiate(dot, spawnPoint, Quaternion.identity);
+        }
+    }
+}
